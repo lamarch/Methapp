851181597762000e5e7a5a541ad852e282798365
lamarch <42736089+p0lohx@users.noreply.github.com>
2020-03-04 17:16:43 +0100

    update gitignore

---
+++ Methapp/Classes/Classes.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Methapp.Classes
+{
+    public class Exploitation : INotifyPropertyChanged
+    {
+        private string nom;
+        private ObservableCollection<Benne> bennes;
+        private ObservableCollection<Champ> champs;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Nom {
+            get => this.nom;
+            set {
+                if ( this.nom != value ) {
+                    this.nom = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+        public ObservableCollection<Benne> Bennes {
+            get => this.bennes;
+            set {
+                if ( this.bennes != value ) {
+                    this.bennes = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public ObservableCollection<Champ> Champs { 
+            get => this.champs;
+            set {
+                if(value != champs ) {
+                    this.champs = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private void NotifyPropertyChanged ( [CallerMemberName] string name = "" ) => PropertyChanged?.Invoke(
+                this,
+                new PropertyChangedEventArgs( name ) );
+
+        public Exploitation ()
+        {
+            Bennes = new ObservableCollection<Benne>();
+            Champs = new ObservableCollection<Champ>();
+            Nom = "Nom indéfini";
+        }
+
+    }
+
+    public class Champ : INotifyPropertyChanged
+    {
+        private string nom;
+        private float surface;
+
+        public Champ ( string nom, float surface )
+        {
+            this.nom = nom;
+            this.surface = surface;
+        }
+
+        public Champ () : this( "Nom indéfini", 0f ) { }
+
+        public float Surface {
+            get => this.surface;
+            set {
+                if ( this.surface != value ) {
+                    this.surface = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string Nom {
+            get => this.nom;
+            set {
+                if ( value != this.nom ) {
+                    this.nom = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private void NotifyPropertyChanged ( [CallerMemberName] string name = "" ) => PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( name ) );
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+
+    public class Benne : INotifyPropertyChanged
+    {
+        private int id;
+        private float tare;
+
+
+        public float Matiere ( float poids_chargée ) => poids_chargée - Tare;
+
+        public float Tare {
+            get => this.tare;
+            set {
+                if ( this.tare != value ) {
+                    this.tare = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+        public int Id {
+            get => this.id;
+            set {
+                if ( this.id != value ) {
+                    this.id = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private void NotifyPropertyChanged ( [CallerMemberName] string name = "" ) => PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( name ) );
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Benne ()
+        {
+            this.id = 1;
+            this.tare = 0;
+        }
+
+    }
+}
--- Methapp/MainWindow.xaml.cs
+++ Methapp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,28 +20,68 @@ namespace Methapp
 {
 
 
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
 
         public MainWindow ()
         {
             DataContext = this;
-            Clients = new ObservableCollection<Client>();
-            Clients.Add( new Client() { Nom = "Client_test", Proprietaire = "jack de b.", Bennes = new List<Benne>() { new Benne() { Id = 125 }, new Benne() { Id = 15 } } } );
-            Clients.Add( new Client() { Nom = "Client_test", Proprietaire = "jack de b.", Bennes = new List<Benne>() { new Benne() { Id = 125 }, new Benne() { Id = 15 } } } );
+            Exploitations = new ObservableCollection<Exploitation>();
+            Exploitations.Add( new Exploitation() {
+                Nom = "ferme A",
+                Bennes = new ObservableCollection<Benne>() { new Benne() { Id = 0001 } },
+                Champs = new ObservableCollection<Champ>() { new Champ( "RGMT", 5.14f ) }
+            } );
 
-            lv_clients.SelectionChanged += (object sender, SelectionChangedEventArgs e) => {
-                
-            }
-
+            Exploitations.Add( new Exploitation() {
+                Nom = "ferme B",
+                Bennes = new ObservableCollection<Benne>() { new Benne() { Id = 2 } },
+                Champs = new ObservableCollection<Champ>() { new Champ() }
+            } );
             InitializeComponent();
 
+            lv_exploitations.SelectionChanged += ( object sender, SelectionChangedEventArgs e ) => {
+                PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( "Selected_expl" ) );
+            };
+            lv_exploitations.SelectedItem = lv_exploitations.Items[0];
+
         }
 
-        public Client Selected_client {
-            get => lv_clients.SelectedItem as Client;
+        public Exploitation Selected_expl {
+            get => lv_exploitations.SelectedItem as Exploitation;
         }
 
-        public ObservableCollection<Client> Clients { get; set; }
+        public ObservableCollection<Exploitation> Exploitations { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void bt_add_expl_Click ( object sender, RoutedEventArgs e )
+        {
+            // add new client
+            Exploitations.Add( new Exploitation() );
+            lv_exploitations.SelectedItem = lv_exploitations.Items[lv_exploitations.Items.Count - 1];
+        }
+
+        private void bt_del_expl_Click ( object sender, RoutedEventArgs e )
+        {
+            if(lv_exploitations.SelectedItem != null ) {
+                Exploitations.Remove( (Exploitation)lv_exploitations.SelectedItem );
+            }
+        }
+
+        private void bt_add_benne_Click ( object sender, RoutedEventArgs e )
+        {
+            if(Selected_expl != null ) {
+                Selected_expl.Bennes.Add( new Benne() );
+                lv_bennes.SelectedItem = lv_bennes.Items[lv_bennes.Items.Count - 1];
+            }
+        }
+
+        private void bt_del_benne_Click ( object sender, RoutedEventArgs e )
+        {
+            if(Selected_expl != null && lv_bennes.SelectedItem != null ) {
+                Selected_expl.Bennes.Remove( (Benne)lv_bennes.SelectedItem );
+            }
+        }
     }
 }
